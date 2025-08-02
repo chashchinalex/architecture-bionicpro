9f91a79fc810aec7de5adcb9af5b32b78d947596
AlexanderSkySych <SkySych@yandex.ru>
2025-08-02 22:01:42 +0300

    Fix cors error

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 from uuid import uuid4
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title="BionicPro"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/reports")
