60861e74da49d4a3558c3c45c929e235bbcf1cfb
Дмитрий Магала <d.magala@vk.team>
2025-11-04 06:41:16 +0300

    Sprint 9 Task 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,8 @@ import ReportPage from './components/ReportPage';
 const keycloakConfig: KeycloakConfig = {
   url: process.env.REACT_APP_KEYCLOAK_URL,
   realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||"",
+  pkceMethod: 'S256'
 };
 
 const keycloak = new Keycloak(keycloakConfig);
