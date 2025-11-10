ba7c0891fbeb64d969fa001dc377a08298057a40
Denis <omelched@gmail.com>
2025-11-10 12:51:34 +0300

    Задание 3

--- bionicpro-auth/bionicpro_auth/api/reports.py
+++ bionicpro-auth/bionicpro_auth/api/reports.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Cookie
 
 from bionicpro_auth.services.keycloak import KeycloakOpenIdService
 from bionicpro_auth.services.reports import ReportService
+from bionicpro_auth.services.s3_storage import S3Storage
 
 router = APIRouter(prefix="/reports")
 
@@ -13,9 +14,15 @@ async def get_my_report(
     session_key: Annotated[str | None, Cookie()] = None,
 ):
     koi_service = KeycloakOpenIdService()
+    s3_storage = S3Storage()
     if not session_key:
         raise ValueError
 
     username = await koi_service.get_username(session_key)
-    with ReportService() as report_service:
-        return report_service.load_report(username)
+
+    if not (report_url := s3_storage.find_report(username)):
+        with ReportService() as report_service:
+            report_data = report_service.load_report(username)
+        return s3_storage.load_report(username, report_data)
+
+    return report_url.replace("s3", "localhost")
